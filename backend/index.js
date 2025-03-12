8dbbd3c5df3d2dd1afaeac2ff481fccecb914d8a
freshmint <cd62957@gmail.com>
2025-03-12 12:18:28 +0000

    Modificação do backend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,43 @@
-const http = require('http')
-const port = 8080
+const express = require('express')
+const mqtt = require('mqtt')
+const jwt  = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const Pool = require('pg').Pool
+const dotenv = require('dotenv')
+const app = express()
+const port = 3000
 
-const server = http.createServer(function (req, res) {
+app.use(express.json())
 
-    res.write('Hello World')
-
-    res.end()
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host:process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password:process.env.DB_PASSWORD,
+  port: 5432,
 })
 
-server.listen(port, function (error) {
+const client = mqtt.connect('mqtt://broker.hivemq.com', { 
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD
+});
 
-  
-    if (error) {
-        console.log('Something went wrong', error);
-    }
+const topic = "hello";
 
-    else {
-        console.log('Server is listening on port ' + port);
-    }
+client.on('connect', async () => {
+  console.log('Connected to MQTT broker');
+  client.subscribe(topic);
+});
+
+client.on('message', (topic, message) => {
+  console.log(`Received message on topic: `, topic, 'message: ', message);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
---
+++ db/init.sql
@@ -0,0 +1,7 @@
+-- Create table users
+CREATE TABLE seller_categories (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT,
+);
+
