26ab327616fe93cf2784ae28bc96c8f00dfc6d0f
freshmint <cd62957@gmail.com>
2025-03-13 08:17:48 +0000

    Começo do MQTT

--- backend/index.js
+++ backend/index.js
@@ -9,29 +9,39 @@ const port = 3000
 
 app.use(express.json())
 
+dotenv.config();
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host:process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password:process.env.DB_PASSWORD,
   port: 5432,
-})
+});
 
 const client = mqtt.connect('mqtt://broker.hivemq.com', { 
   username: process.env.MQTT_USERNAME,
   password: process.env.MQTT_PASSWORD
 });
 
-const topic = "hello";
-
 client.on('connect', async () => {
   console.log('Connected to MQTT broker');
-  client.subscribe(topic);
 });
 
 client.on('message', (topic, message) => {
   console.log(`Received message on topic: `, topic, 'message: ', message);
 });
+
+/*app.post('/login', async (req, res) => {
+  try{
+    const {email, password} = req.body;
+
+    const [userResult] = await pool.query({
+      text: 'SELECT * FROM users WHERE email = $1',
+      values: [email]
+    });
+
+  }*/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
