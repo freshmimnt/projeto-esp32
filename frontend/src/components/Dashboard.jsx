4db9ba25a4a8944a0a9a9dff8c402e708e7cab8e
António Santos <cd62957@gmail.com>
2025-05-29 23:29:55 +0100

    Código final

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -8,7 +8,6 @@ const Dashboard = () => {
 
 
   const handleLogout = () => {
-    // to clear the localStorage
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     navigate("/");
@@ -36,43 +35,3 @@ const Dashboard = () => {
 export default Dashboard;
 
 
-
-
-
-/*import StatsDisplay from "./StatsDisplay";
-import ControlPanel from "./ControlPanel";
-import { useNavigate } from "react-router-dom";
-
-import "../styles/dashboard.css";
-
-const Dashboard = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    // Clear localStorage
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    navigate("/");
-  };
-
-  return (
-    <div className="dashboard-container">
-      <div className="dashboard-header">
-        <h2>Dashboard</h2>
-        <div className="header-controls">
-          <button onClick={handleLogout} className="logout-btn">
-            Logout
-          </button>
-        </div>
-      </div>
-
-      <div className="dashboard-content">
-        <StatsDisplay />
-        <ControlPanel />
-      </div>
-    </div>
-  );
-};
-
-export default Dashboard;
-*/
